ee95e196762e557a6083f31e02e22de0aa06cfe8
Colin M. Leung <colinmleung@gmail.com>
2012-10-15 22:27:27 -0700

    Uses new database table for text data.

--- journalpage.php
+++ journalpage.php
@@ -13,17 +13,31 @@
 	//echo '<p>post data = '.$_POST['submit'].'</p>';
 	
 	if(isset($_POST['submit'])) {
+	
+			// check for existing dated workjournal page
+			$query = "SELECT * FROM workjournal_data WHERE user_id='".$_SESSION['user_id']."'";
+			$data = mysqli_query($dbc, $query);
+			$row = mysqli_fetch_array($data);
+			
+			// If no date exists
+			if (mysqli_num_rows($data) != 1) {
+				// create a record of one
+				$query = "INSERT INTO workjournal_data (user_id, date) VALUES ('".$_SESSION['user_id']."', CURDATE())";
+				mysqli_query($dbc, $query);
+			}
+			
+			// Transfer data to variables
 			$tasksp = $_POST['tasksp'];
 			$tasksc = $_POST['tasksc'];
 			$issues = $_POST['issues'];
 			
 			// Send text information to database
-			$query = "UPDATE workjournal_user SET tasksp = '".$tasksp."', tasksc = '".$tasksc."', issues = '".$issues."' WHERE user_id ='".$_SESSION['user_id']."'";
+			$query = "UPDATE workjournal_data SET tasksp = '".$tasksp."', tasksc = '".$tasksc."', issues = '".$issues."' WHERE user_id ='".$_SESSION['user_id']."'";
 			mysqli_query($dbc, $query);
 	}
 	
 	// Get text information from database
-	$query = "SELECT tasksp, tasksc, issues FROM workjournal_user WHERE user_id='".$_SESSION['user_id']."'";
+	$query = "SELECT tasksp, tasksc, issues FROM workjournal_data WHERE user_id='".$_SESSION['user_id']."'";
 	$data = mysqli_query($dbc, $query);
 	$row = mysqli_fetch_array($data);
 	
@@ -39,6 +53,8 @@
 			<input type="submit" value="Save" name="submit">
 </form>
 
+<a href="forward.php">Forward</a>
+<a href="backward.php">Backward</a>
 <a href="signout.php">Log Out</a>
 
 <?php
