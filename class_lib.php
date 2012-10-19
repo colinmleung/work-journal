5bd1296a83e0bcf6542dbfc95980a7fcd57ed6cc
Colin M. Leung <colinmleung@gmail.com>
2012-10-18 21:38:22 -0700

    Converted existing DB Connection Class to use PDO instead of mysqli.

--- class_lib.php
+++ class_lib.php
@@ -4,14 +4,16 @@
 		private $db_conn;
 		
 		function __construct($db_host, $db_user, $db_password, $db_name) {
-			$this->db_conn = new mysqli($db_host, $db_user, $db_password, $db_name);
-			if ($this->db_conn->connect_error) {
-				die('Connect Error (' . $this->db_conn->connect_errno . ') ' . $this->db_conn->connect_error); 
+			try {
+				$db_conn = new PDO($db_name, $db_host, $db_user, $db_password);
+				$db_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			} catch(PDOException $e) {
+				echo $e->getMessage();
 			}
 		}
 		
 		function getData($query) {
-			return $this->db_conn->query($query)->fetch_array();
+			return $this->db_conn->query($query)->fetchAll();
 		}
 		
 		function query($query) {
@@ -19,12 +21,18 @@
 		}
 		
 		function getNumRows($query) {
-			return $this->db_conn->query($query)->num_rows;
+			return $this->db_conn->query($query)->rowCount();
 		}
 		
-		function validateString($string) {
-			return $this->db_conn->real_escape_string($string);
+		function validateUserLogin($username, $password) {
+			$db_conn->beginTransaction();
+			$query = $db_conn->prepare("SELECT user_id FROM workjournal_user WHERE username = :username and password = :password");
+			$query->bindParam(':username', $username, PDO::PARAM_STR);
+			$query->bindParam(':password', $password, PDO::PARAM_STR);
+			$query->execute();
+			$db_conn->commit();
 		}
+		
 		
 	}
 	
