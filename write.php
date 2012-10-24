cbd1c45b70203fbaccda91801ede167c432c147c
Colin M. Leung <colinmleung@gmail.com>
2012-10-24 00:00:57 -0700

    Added main application pages.

---
+++ write.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Work Journal - PLACEHOLDER_FOR_CURRENT_DATE</title>
+		<meta name="description" content="A place to think about your work. Work Journal is a questionnaire creator that improves your productivity by getting you to think about the questions that really matter."/>
+	</head>
+	<body>
+		<header>
+			<h1>PLACEHOLDER_FOR_CURRENT_DATE</h1>
+		</header>
+		<nav>
+			<a href="write.php">Write</a>
+			<a href="read.php">Read</a>
+			<a href="templates.php">Templates</a>
+			<a href="logout.php">Log Out</a>
+		</nav>
+		<div id="journal">
+			<div id="actionBar">
+			<form method="post" action="write.php">
+				<input type="submit" value="Save" name="save"/>
+				<input type="submit" value="Clear" name="clear"/>
+			</form>
+			</div>
+			<!-- add php code here for template loading -->
+		</div>
+	</body>
+</html>
