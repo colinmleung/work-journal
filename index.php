78edabd858ae4add0a65853086e58a4eba2c980a
Colin M. Leung <colinmleung@gmail.com>
2012-10-23 22:20:28 -0700

    Added home page html file.

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Work Journal - Record and reflect on your work</title>
+		<meta name="description" content="A place to think about your work. Work Journal is a questionnaire creator that improves your productivity by getting you to think about the questions that really matter."/>
+		
+		<!--[if IE]> 
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]--> 
+		<!--[if lte IE 7]> 
+			<script src="js/IE8.js" type="text/javascript"></script>
+		<![endif]--> 
+		<!--[if lt IE 7]> 
+			<link rel="stylesheet" type="text/css" media="all" href="css/ie6.css"/>
+		<![endif]-->
+
+	</head>
+	<body>
+		<header>
+				<h1>Work Journal</h1>
+				<p>A place to think about your work.</p>
+		</header>
+		<div id="features">
+			<div id="record">
+				<h2>Record</h2>
+				<p>Preserve your thoughts and feelings right when they happen.</p>
+			</div>
+			<div id="reflect">
+				<h2>Reflect</h2>
+				<p>Get a bird's-eye view of your work and identify key issues.</p>
+			</div>
+			<div id="templates">
+				<h2>Custom Templates</h2>
+				<p>Create questionnaires to ask yourself those important questions day after day.</p>
+			</div>
+		</div>
+		<div id="signUpButton">
+			<button type="button">Sign Up</button>
+		</div>
+		<div id="loginForm">
+			<form name="login" action="" method="post">
+				<label>Username</label>
+				<input name="username" type="text"/>
+				<label>Password</label>
+				<input name="password" type="text"/>
+				<input name="Log In" type="submit"/>
+			</form>
+		</div>
+	</body>
+</html>
