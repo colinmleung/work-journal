c0a56e33f618e50a579aeabab99c38bf98d14e62
Colin M. Leung <colinmleung@gmail.com>
2012-11-05 18:36:06 -0800

    Implemented MVC with abstract factory design pattern. Delegate redirecting to utility class.

---
+++ journalpageold.php
@@ -0,0 +1,73 @@
+<?php
+	require_once('startsession.php');
+	require_once('header.php');
+	require_once('constants.php');
+	require_once('class_lib.php');
+	
+	if(isset($_POST['forward'])) {
+		$_SESSION['curdate'] = date("Y-m-d", strtotime($_SESSION['curdate'])+DAY);
+	}
+	
+	if(isset($_POST['backward'])) {
+		$_SESSION['curdate'] = date("Y-m-d", strtotime($_SESSION['curdate'])-DAY);
+	}
+	
+	// Connect to the database
+	$dbc = new DAO;
+	
+	$tasksp = '';
+	$tasksc = '';
+	$issues = '';
+	
+	echo '<p>date = '.$_SESSION['curdate'].'</p>';
+	
+	if(isset($_POST['submit'])) {
+	
+			// check for existing dated workjournal page
+			$query = "SELECT * FROM workjournal_data WHERE user_id ='".$_SESSION['user_id']."' AND date = '".$_SESSION['curdate']."'";
+			$qro = new QRO($dbc->query($query));
+			$row = $qro->fetchArray();
+			
+			// If no date exists
+			if ($qro->numRows == NO_RECORDS) {
+				// create a record of one
+				$query = "INSERT INTO workjournal_data (user_id, date) VALUES ('".$_SESSION['user_id']."', '".$_SESSION['curdate']."')";
+				$dbc->query($query);
+			}
+			
+			// Transfer data to variables
+			$tasksp = $_POST['tasksp'];
+			$tasksc = $_POST['tasksc'];
+			$issues = $_POST['issues'];
+			
+			// Send text information to database
+			$query = "UPDATE workjournal_data SET tasksp = '".$tasksp."', tasksc = '".$tasksc."', issues = '".$issues."' WHERE user_id ='".$_SESSION['user_id']."' AND date = '".$_SESSION['curdate']."'";
+			$dbc->query($query);
+	}
+	
+	// Get text information from database
+	$query = "SELECT tasksp, tasksc, issues FROM workjournal_data WHERE user_id ='".$_SESSION['user_id']."' AND date = '".$_SESSION['curdate']."'";
+	$qro = new QRO($dbc->query($query));
+	//echo '<p>data = '.$data.'</p>';
+	$row = $qro->fetchArray();
+	
+	$tasksp = $row['tasksp'];
+	$tasksc = $row['tasksc'];
+	$issues = $row['issues'];
+?>
+
+<form name="journal" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+			Tasks Planned: <textarea name="tasksp" id="tasksp" rows="3" cols="30"><?php echo $tasksp ?></textarea>
+			Tasks Completed: <textarea name="tasksc" id="tasksc" rows="3" cols="30"><?php echo $tasksc ?></textarea>
+			Issues: <textarea name="issues" id="issues" rows="3" cols="30"><?php echo $issues ?></textarea>
+			<input type="submit" value="Save" name="submit"/>
+			<input type="submit" value="Forward" name="forward"/>
+			<input type="submit" value="Backward" name="backward"/>
+</form>
+
+<a href="forward.php">Forward</a>
+<a href="backward.php">Backward</a>
+<a href="signout.php">Log Out</a>
+
+	</body>
+</html>
---
+++ signupold.php
@@ -0,0 +1,51 @@
+<?php
+	require_once('constants.php');
+	require_once('class_lib.php');
+
+	$dbc = new DAO;
+	
+	if (isset($_POST['submit'])) {
+		$username = $dbc->sanitizeString($_POST['username']);
+		$password = $dbc->sanitizeString($_POST['password']);
+		$password2 = $dbc->sanitizeString($_POST['password2']);
+		
+		
+		if (!empty($username) && !empty($password) && !empty($password2) && ($password == $password2)) {
+			$query = "SELECT * FROM workjournal_user WHERE username='$username'";
+			
+			$qro = new QRO($dbc->query($query));
+			if ($qro->numRows() == NO_RECORDS) {
+				$query = "INSERT INTO workjournal_user (username, password) VALUES ('$username', SHA('$password'))";
+				$dbc->query($query);
+				$home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/signin.php';
+				header('Location: ' . $home_url);
+				exit();
+			} else {
+				echo '<p>Username already taken. Please try another one.</p>';
+			}
+		} else {
+			echo '<p>Please enter the desired username and the desired password twice.</p>';
+		}
+	}
+?>
+
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Sign Up for Work Journal</title>
+		<meta name="description" content="A place to think about your work. Work Journal is a questionnaire creator that improves your productivity by getting you to think about the questions that really matter."/>
+	</head>
+	<body>
+		<p>Sign Up</p>
+		<form name="signUpForm" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+			<label for="username">Username:</label>
+			<input type="text" id="username" name="username" required="required"/>
+			<label for="password">Password:</label>
+			<input type="password" id="password" name="password" required="required"/>
+			<label for="password2">Retype Password:</label>
+			<input type="password" id="password2" name="password2" required="required"/>
+			<input type="submit" value="Submit" name="submit"/>
+		</form>
+	</body>
+</html>
